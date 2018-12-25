69a0acd21596d62d3e8574496f4159f261166d50
NowhereMan1904 <com_89@hotmail.com>
2018-12-25 17:38:54 +0100

    afsd

--- mypyside2.py
+++ mypyside2.py
@@ -4,15 +4,22 @@ from PySide2 import QtCore, QtWidgets, QtGui
 class Window(QtWidgets.QLabel):
     def __init__(self):
         super().__init__()
-
         self.setText("Hello World")
         self.setAlignment(QtCore.Qt.AlignHCenter)
+        self.resize(800,600)
+
+class Coin(QtCore.QObject):
+    new_line = QtCore.Signal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.window = Window()
+        self.new_line.connect(self.window.setText)
+        self.new_line.emit("dlroW olleH")
+        self.window.show()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-
-    window = Window()
-    window.resize(800,600)
-    window.show()
-
+    coin = Coin()
     sys.exit(app.exec_())
